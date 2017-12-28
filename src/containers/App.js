b8a9672eef083174d4e3cf501aa506917b8f02c8
Khai Phan <kphan95@bu.edu>
2017-12-28 17:39:33 -0500

    Playtest with colors

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import logo from '../images/logo.svg';
 import './App.css';
+import headerBackground from '../images/MainHeader.svg';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img className="header-banner-bg" src={headerBackground}></img>
+        
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
